e5873ed2eac4360b112a99ba4cb10be69daaff07
Nika Mamniashvili <36672696+mamniashvili24@users.noreply.github.com>
2024-04-21 05:06:50 +0400

    Init

---
+++ all-down.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Loop through each subdirectory in the current directory
+for dir in */; do
+    # Change directory to the subdirectory
+    cd "$dir" || exit
+    # Loop through each YAML file in the subdirectory
+    for file in *.yml; do
+        # Run Docker Compose for the YAML file
+        docker-compose -f "$file" down
+    done
+    # Change directory back to the parent directory
+    cd ..
+done
